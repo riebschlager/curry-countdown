d73faa9c74bdc68019a607f1a104c4b7f978da1a
Chris Riebschlager <riebschlager@gmail.com>
2017-01-04 21:35:51 -0600

    all fixed up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 var app = angular.module('CurryApp', []);
 
-app.controller('CurryCtrl', function($scope, $interval) {
+app.controller('CurryCtrl', function($scope, $interval, $http) {
     var lastCurryLunch = moment('2016-09-23 12:00');
     var nextCurryLunch = moment(lastCurryLunch).add(4, 'months');
+    var currentImage = 0;
     $scope.lastCurryLunchDisplay = lastCurryLunch.format("dddd, MMMM Do YYYY");
     $scope.nextCurryLunchDisplay = nextCurryLunch.format("dddd, MMMM Do YYYY");
     $scope.nextCurryLunchDistance = nextCurryLunch.fromNow(true);
+
+    $http.get('http://api.giphy.com/v1/gifs/search?q=indian+food&api_key=dc6zaTOxFJmzC').then(function(res) {
+        var images = res.data.data;
+        $scope.image = images[Math.floor(Math.random() * images.length)];
+        $interval(function() {
+            $scope.image = images[Math.floor(Math.random() * images.length)];
+        }, 4 * 1000);
+    });
 });
