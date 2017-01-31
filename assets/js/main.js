51adb1f3258e34b571b70ca70597f7fb34c0df57
Chris Riebschlager <riebschlager@gmail.com>
2017-01-31 10:09:49 -0600

    it's curry time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,23 @@
-var app = angular.module('CurryApp', []);
+const app = angular.module('CurryApp', []);
 
 app.controller('CurryCtrl', function($scope, $interval, $http) {
-    var lastCurryLunch = moment('2016-09-23 12:00');
-    var nextCurryLunch = moment(lastCurryLunch).add(4, 'months');
-    var currentImage = 0;
+
+    const lastCurryLunch = moment('2016-09-23 12:00');
+    const nextCurryLunch = moment(lastCurryLunch).add(4, 'months');
+
+    let currentImage = 0;
+
     $scope.lastCurryLunchDisplay = lastCurryLunch.format("dddd, MMMM Do YYYY");
     $scope.nextCurryLunchDisplay = nextCurryLunch.format("dddd, MMMM Do YYYY");
     $scope.nextCurryLunchDistance = nextCurryLunch.fromNow(true);
 
+    $scope.isCurryTime = moment().isSameOrAfter(nextCurryLunch);
+
     $http.get('https://api.giphy.com/v1/gifs/search?q=indian+food&api_key=dc6zaTOxFJmzC').then(function(res) {
-        var images = res.data.data;
+        let images = res.data.data;
         $scope.image = images[Math.floor(Math.random() * images.length)];
         $interval(function() {
             $scope.image = images[Math.floor(Math.random() * images.length)];
-        }, 4 * 1000);
+        }, 2 * 1000);
     });
 });
